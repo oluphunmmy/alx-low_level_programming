2c34784b4b5e97ba6ebdec89dd6216520f16afdd
oluphunmmy <olufunmilayoagboola@gmail.com>
2022-12-21 12:57:09 -0800

    concatenates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	char *s = dest;
 
-	i = 0;
-	while (dest[i] != '\0')
-
+	while (*dest != '\0')
 	{
-		i++;
+		dest++;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
 
-	return (dest);
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
