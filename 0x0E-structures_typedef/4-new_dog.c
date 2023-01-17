325c85316ee8198ae774cfe98c7ca2c2b15f0053
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-01-16 20:58:22 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,21 +1,21 @@
 #include <stdlib.h>
 	 #include "dog.h"
 
-	/**
-	 * _strlen - returns the length of a string
-	 * @s: string to evaluate
-	 *
-	 * Return: the length of the string
-	 */
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
 	int i;
 	 i = 0;
-	 while (s[i] != '\0')
-	 {
-		 i++;
-	 }
-	 return (i);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 /**
  *  *_strcpy - copies the string pointed to by src
@@ -30,16 +30,16 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 	 len = 0;
-	 while (src[len] != '\0')
-	 {
-		 len++;
-	 }
-	 for (i = 0; i < len; i++)
-	 {
-		 dest[i] = src[i];
-	 }
-	 dest[i] = '\0';
-	 return (dest);
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 /**
  * new_dog - creates a new dog
