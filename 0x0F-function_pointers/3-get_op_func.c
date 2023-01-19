97c80e904782de94a7cbdf8aefd9ce75599162b1
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-01-18 20:24:23 -0800

    simple operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,7 +22,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
-	return (ops[i].f);
+	while (i < 5)
 	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+
+		i++;
+	}
+		return (0);
+}
