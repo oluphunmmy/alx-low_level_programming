867c368bf88acacf4525ac4d2e9aa65997edc9f4
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-01-18 20:32:16 -0800

    operation

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,9 +1,6 @@
 #include "3-calc.h"
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <stdlib.h>
+#include <stdio.h>
 
 
 /**
