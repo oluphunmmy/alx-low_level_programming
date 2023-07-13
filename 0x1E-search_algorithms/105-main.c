75447727b7234dfb53e5b4552d3b7fa47c253229
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-07-12 21:58:47 -0700

    sqrt

---
+++ 0x1E-search_algorithms/105-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
